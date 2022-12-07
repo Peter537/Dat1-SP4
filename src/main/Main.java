78fb36039118ae649fa8995555a8e1e70dc982ef
Peter537 <pean@outlook.dk>
2022-12-07 19:53:45 +0100

    main flow start and database password getter

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,23 @@
 package main;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FormulaOne formulaOne = new FormulaOne(getPassword());
+        formulaOne.run();
+    }
+
+    public static String getPassword() {
+        File file = new File("Data/databasepassword.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            return scanner.nextLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
