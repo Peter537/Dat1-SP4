3d2e4bb8c06bb322198c2116be0b416f2fb05ff2
Peter537 <pean@outlook.dk>
2022-12-07 19:53:54 +0100

    started formula one class

--- src/main/FormulaOne.java
+++ src/main/FormulaOne.java
@@ -1,7 +1,38 @@
 package main;
 
+import main.data.ISessionCache;
+import main.data.impl.SessionCacheImpl;
+
+import javax.swing.*;
+
 public class FormulaOne {
 
+    private final ISessionCache sessionCache;
+    private static JFrame page;
 
+    public FormulaOne(String dbPassword) {
+        sessionCache = new SessionCacheImpl();
+        loadDB(dbPassword);
+    }
 
+    private void loadDB(String dbPassword) {
+        // TODO
+    }
+
+    public void run() {
+        chooseSaveGame();
+        // TODO
+    }
+
+    private void chooseSaveGame() {
+        // TODO
+    }
+
+    public ISessionCache getSessionCache() {
+        return this.sessionCache;
+    }
+
+    public static JFrame getPage() {
+        return FormulaOne.page;
+    }
 }
