dc1eabf6f09a32115fadf1157dcd9963298c35b5
Yusuf Al-Khafaji <113047451+StylizedAce@users.noreply.github.com>
2022-12-08 07:00:48 +0100

    Was i supposed to work into sessioncache and season?
    I did this anyway and it is ofc reversible. FormulaOne class needs some touchups if i had done this correctly, as sessioncache does not have its parameters in FormulaOne

--- src/main/data/impl/SeasonImpl.java
+++ src/main/data/impl/SeasonImpl.java
@@ -1,9 +1,26 @@
 package main.data.impl;
 
 import main.data.ISeason;
+import main.data.ITeam;
+import main.race.IRace;
+import main.utils.DriverLeaderboard;
+import main.utils.TeamLeaderboard;
+
+import java.util.ArrayList;
 
 public class SeasonImpl implements ISeason {
+private final int year;
+private final ArrayList<IRace> races;
+private final ArrayList<ITeam> teams;
+private final TeamLeaderboard teamLeaderboard;
+private final DriverLeaderboard driverLeaderboard;
 
-
-
+    public SeasonImpl(int year, ArrayList<IRace> races, ArrayList<ITeam> teams, TeamLeaderboard teamLeaderboard, DriverLeaderboard driverLeaderboard) {
+        this.year = year;
+        this.races = races;
+        this.teams = teams;
+        this.teamLeaderboard = teamLeaderboard;
+        this.driverLeaderboard = driverLeaderboard;
+    }
+    // TODO: finish this class with the bottompart of UML scheme (of the Season class)
 }
--- src/main/data/impl/SessionCacheImpl.java
+++ src/main/data/impl/SessionCacheImpl.java
@@ -1,9 +1,27 @@
 package main.data.impl;
 
+import main.FormulaOne;
+import main.data.ISeason;
 import main.data.ISessionCache;
 
+import java.util.ArrayList;
+
 public class SessionCacheImpl implements ISessionCache {
+private final FormulaOne formulaOne;
+private final int saveID;
+private ArrayList<ISeason> seasons;
+private SeasonImpl season;
 
+    public SessionCacheImpl(FormulaOne formulaOne, int saveID) {
+        this.formulaOne = formulaOne;
+        this.saveID = saveID;
+    }
 
+    public int getSaveID() {
+        return saveID;
+    }
+    public ArrayList<ISeason> getSeasons() {
+        return seasons;
+    }
 
 }
