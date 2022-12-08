5d58da061ff9fd79aa8bc8ea0ee66939e4d9c846
Yusuf Al-Khafaji <113047451+StylizedAce@users.noreply.github.com>
2022-12-08 11:58:39 +0100

    Added attributes to driver and car (impl)

--- src/main/data/impl/CarImpl.java
+++ src/main/data/impl/CarImpl.java
@@ -7,9 +7,21 @@ public class CarImpl implements ICar {
     private final int id;
     private final String name;
 
-    public CarImpl(int id, String name) {
+    private int horsePower;
+
+    private int weight;
+
+    private int aerodynamics;
+
+    private int traction;
+
+    public CarImpl(int id, String name, int horsePower, int weight, int aerodynamics, int traction) {
         this.id = id;
         this.name = name;
+        this.horsePower = horsePower;
+        this.weight = weight;
+        this.aerodynamics = aerodynamics;
+        this.traction = traction;
     }
 
     @Override
--- src/main/data/impl/DriverImpl.java
+++ src/main/data/impl/DriverImpl.java
@@ -9,10 +9,22 @@ public class DriverImpl implements IDriver {
     private final int teamID;
     private int point;
 
-    public DriverImpl(int id, String name, int teamID) {
+    private int experience;
+
+    private int corner;
+
+    private int consistency;
+
+    private int acceleration;
+
+    public DriverImpl(int id, String name, int teamID, int experience, int corner, int consistency, int acceleration) {
         this.id = id;
         this.name = name;
         this.teamID = teamID;
+        this.experience = experience;
+        this.corner = corner;
+        this.consistency = consistency;
+        this.acceleration = acceleration;
     }
 
     @Override
