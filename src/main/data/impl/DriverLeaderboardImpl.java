fbd8406465b83de063baf0cf7a3b787f1383c2a6
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-08 11:21:15 +0100

    created leaderboards

--- src/main/data/impl/DriverLeaderboardImpl.java
+++ src/main/data/impl/DriverLeaderboardImpl.java
@@ -1,7 +1,31 @@
 package main.data.impl;
 
+import main.data.IDriver;
+
+import java.util.ArrayList;
+
 public class DriverLeaderboardImpl {
 
+    private final ArrayList<IDriver> leaderboard;
 
+    public DriverLeaderboardImpl(ArrayList<IDriver> drivers) {
+        this.leaderboard = drivers;
+        update();
+    }
 
+    public void update() {
+        leaderboard.sort((o1, o2) -> Integer.compare(o2.getPoints(), o1.getPoints()));
+    }
+
+    public int getPlacement(IDriver driver) {
+        return leaderboard.indexOf(driver) + 1;
+    }
+
+    public IDriver getByPlacement(int placement) {
+        return leaderboard.get(placement - 1);
+    }
+
+    public ArrayList<IDriver> getLeaderboard() {
+        return leaderboard;
+    }
 }
--- src/main/data/impl/TeamLeaderboardImpl.java
+++ src/main/data/impl/TeamLeaderboardImpl.java
@@ -1,7 +1,31 @@
 package main.data.impl;
 
+import main.data.ITeam;
+
+import java.util.ArrayList;
+
 public class TeamLeaderboardImpl {
 
+    private final ArrayList<ITeam> leaderboard;
 
+    public TeamLeaderboardImpl(ArrayList<ITeam> teams) {
+        this.leaderboard = teams;
+        update();
+    }
 
+    public void update() {
+        leaderboard.sort((o1, o2) -> Integer.compare(o2.getPoints(), o1.getPoints()));
+    }
+
+    public int getPlacement(ITeam team) {
+        return leaderboard.indexOf(team) + 1;
+    }
+
+    public ITeam getByPlacement(int placement) {
+        return leaderboard.get(placement - 1);
+    }
+
+    public ArrayList<ITeam> getLeaderboard() {
+        return leaderboard;
+    }
 }
