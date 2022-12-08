77f2bfb7e5c286c61a5bf055f5dcfb53027c22a9
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2022-12-08 12:26:06 +0100

    creating the basis for race algorithm - now with actual equations

--- src/main/race/RaceAlgorithm.java
+++ src/main/race/RaceAlgorithm.java
@@ -2,12 +2,12 @@ package main.race;
 
 public class RaceAlgorithm
 {
-double currentSpeed = 0;
-double currentAcceleration = 0;
-double distance = 0;
-double time = 0;
-double dtime = 0.1;
-double maxdistance = 1000;
+double currentSpeed = 0; // current speed of the car measured in m/s
+double currentAcceleration = 0; // current acceleration of the car measured in m/s^2
+double distance = 0; // distance traveled by the car measured in m
+double time = 0; //current time os race measured in s
+double dtime = 0.1; //time step measured in s
+double maxdistance = 1000; //max length of race measured in m
 
 
 private void runrace()
@@ -15,7 +15,7 @@ private void runrace()
 	while (distance < maxdistance)
 	{
 		currentAcceleration = getAcceleration(currentSpeed);
-		currentSpeed = currentSpeed + currentAcceleration * dtime;
+		currentSpeed = getSpeed(currentSpeed, currentAcceleration);;
 		distance = distance + currentSpeed * dtime;
 		time = time + dtime;
 	}
@@ -27,9 +27,14 @@ private double getAcceleration(double currentSpeed)
 	double mass = 1000; // 1000 kg
 	double drag = 0.1; // 10% drag
 	double rollresistance = 0.1; // 10% roll resistance
-	double acceleration = (power - drag * currentSpeed * currentSpeed - rollresistance * currentSpeed) / mass;
+	double acceleration = (power - drag * currentSpeed * currentSpeed - rollresistance * currentSpeed) / mass; // F = ma -> a = F/m -> a = (P - Dv^2 - Rv) / m
 	return acceleration;
 
 }
+ private double getSpeed(double currentSpeed, double currentAcceleration)
+ {
+	 currentSpeed = currentSpeed + currentAcceleration * dtime;
+ 		 return currentSpeed;
+ }
 
 }
