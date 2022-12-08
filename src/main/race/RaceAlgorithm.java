63854519020d1046972d87ae297871e05735bfc1
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2022-12-08 11:06:27 +0100

    creating the basis for race algorithm

--- src/main/race/RaceAlgorithm.java
+++ src/main/race/RaceAlgorithm.java
@@ -2,39 +2,41 @@ package main.race;
 
 public class RaceAlgorithm
 {
-double currentSpeed = 0; // current speed of the car measured in m/s
-double currentAcceleration = 0; // current acceleration of the car measured in m/s^2
-double distance = 0; // distance traveled by the car measured in m
-double time = 0; //current time os race measured in s
-double dtime = 0.1; //time step measured in s
-double maxdistance = 1000; //max length of race measured in m
+double currentSpeed = 0;
+double currentAcceleration = 0;
+double distance = 0;
+double time = 0;
+double dtime = 0.1;
+double maxdistance = 1000;
 
 
-private void runrace()
+while (distance < maxdistance)
 {
-	while (distance < maxdistance)
-	{
-		currentAcceleration = getAcceleration(currentSpeed);
-		currentSpeed = getSpeed(currentSpeed, currentAcceleration);;
-		distance = distance + currentSpeed * dtime;
-		time = time + dtime;
-	}
+currentAcceleration = getAcceleration(currentSpeed);
+currentSpeed = getSpeed(currentSpeed, currentAcceleration);
+distance = getDistance(distance, currentSpeed);
+time = getTime(time, dtime);
+}
+
+private double getTime(double time, double dtime)
+{
+return time + dtime;
+}
+
+private double getDistance(double distance, double currentSpeed)
+{
+return distance + currentSpeed * dtime;
+}
+
+private double getSpeed(double currentSpeed, double currentAcceleration)
+{
+return currentSpeed + currentAcceleration * dtime;
 }
 
 private double getAcceleration(double currentSpeed)
 {
-	int power = 1000; // 1000 hp
-	double mass = 1000; // 1000 kg
-	double drag = 0.1; // 10% drag
-	double rollresistance = 0.1; // 10% roll resistance
-	double acceleration = (power - drag * currentSpeed * currentSpeed - rollresistance * currentSpeed) / mass; // F = ma -> a = F/m -> a = (P - Dv^2 - Rv) / m
-	return acceleration;
 
 }
- private double getSpeed(double currentSpeed, double currentAcceleration)
- {
-	 currentSpeed = currentSpeed + currentAcceleration * dtime;
- 		 return currentSpeed;
- }
+
 
 }
