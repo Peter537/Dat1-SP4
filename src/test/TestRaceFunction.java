b3f477e2922b10aa250b1f0a24c00b93a4952572
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-13 09:54:28 +0100

    changed the test

--- src/test/TestRaceFunction.java
+++ src/test/TestRaceFunction.java
@@ -1,6 +1,11 @@
 package test;
 
+import main.data.ICar;
+import main.data.IDriver;
 import main.data.ITeam;
+import main.data.impl.CarImpl;
+import main.data.impl.DriverImpl;
+import main.data.impl.TeamImpl;
 import main.database.DataBaseIO;
 import main.race.ICircuit;
 import main.race.IRace;
@@ -12,21 +17,27 @@ import java.util.ArrayList;
 public class TestRaceFunction {
 
     public static void main(String[] args) {
-        DataBaseIO.initSQL(false, null);
-        ArrayList<ITeam> teams = DataBaseIO.loadDefaultTeamData();
+        ArrayList<ITeam> teams = getSomeDefaultData();
 
         ICircuit circuit1 = new CircuitImpl(1, "Circuit1", "Country1", 58, null);
-        ICircuit circuit2 = new CircuitImpl(2, "Circuit2", "Country2", 62, null);
-        ICircuit circuit3 = new CircuitImpl(3, "Circuit3", "Country3", 71, null);
-        ICircuit circuit4 = new CircuitImpl(4, "Circuit4", "Country4", 65, null);
 
         IRace race1 = new RaceImpl(1, circuit1, teams);
-        IRace race2 = new RaceImpl(1, circuit2, teams);
-        IRace race3 = new RaceImpl(1, circuit3, teams);
-        IRace race4 = new RaceImpl(1, circuit4, teams);
-        
+
         race1.nextAction();
         race1.nextAction();
-        race1.nextAction();
+    }
+
+    public static ArrayList<ITeam> getSomeDefaultData() {
+        ArrayList<ITeam> teams = new ArrayList<>();
+
+        // create 10 teams in total
+        for (int i = 1; i <= 10; i++) {
+            ICar car = new CarImpl(i, "car" + i, 1, 1, 1, 1);
+            IDriver driver1 = new DriverImpl(i * 2 - 1, "driver" + (i * 2 - 1), i, 1, 1, 1, 1);
+            IDriver driver2 = new DriverImpl(i * 2, "driver" + (i * 2), i, 1, 1, 1, 1);
+            teams.add(new TeamImpl(i, "team" + i, car, driver1, driver2));
+        }
+
+        return teams;
     }
 }
