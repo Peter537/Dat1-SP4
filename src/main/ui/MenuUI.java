9749036fc915930c78d13b8077e134acd0ee48fa
TheLonelyDude <magnus.kongstad@gmail.com>
2022-12-14 13:19:22 +0100

    Moved stuff into their own methods for space

--- src/main/ui/MenuUI.java
+++ src/main/ui/MenuUI.java
@@ -56,64 +56,9 @@ public class MenuUI extends AUI {
     }
 
     public void setData(MenuUI data) {
-        ArrayList<ITeam> teams = formulaOne.getSessionCache().getTeams(); //TODO: maybe change to teamleaderboard class
-        ArrayList<IDriver> drivers = new ArrayList<>();
-        ArrayList<String> values = new ArrayList<>(); //This can also be done with the team class...
-        // Add header
-        values.add("Points | Team | Driver 1 | Driver 2 | Car");
-        for (ITeam team : teams) {
-            values.add(
-                team.getPoints() + " | " +
-                team.getName() + " | " +
-                team.getDriver1().getName() + " | " +
-                team.getDriver2().getName() + " | " +
-                team.getCar().getName()
-            );
-            drivers.add(team.getDriver1());
-            drivers.add(team.getDriver2());
-        }
-        teamLeaderboard.setListData(values.toArray());
-
-        ArrayList<String> driverValues = new ArrayList<>();
-        driverValues.add("Points | Driver | Team ID | Experience | Acceleration | Consistency | Cornering");
-        for (IDriver driver : drivers) {
-            driverValues.add(
-                driver.getPoints() + " | " +
-                driver.getName() + " | " +
-                driver.getTeamID() + " | " +
-                driver.getExperience() + " | " +
-                driver.getAcceleration() + " | " +
-                driver.getConsistency() + " | " +
-                driver.getCorner()
-            );
-        }
-        driverLeaderboard.setListData(driverValues.toArray());
-
-        if (currentUser != null) {
-            ArrayList<Font> fonts = new ArrayList<>();
-            fonts.add(new Font("Comic Sans MS", Font.BOLD, 28));
-            //Add more fonts with same size here
-            fonts.add(new Font("Arial", Font.BOLD, 28));
-            fonts.add(new Font("Times New Roman", Font.BOLD, 28));
-            fonts.add(new Font("Verdana", Font.BOLD, 28));
-            fonts.add(new Font("Tahoma", Font.BOLD, 28));
-            fonts.add(new Font("Calibri", Font.BOLD, 28));
-            int randomFont = (int) (Math.random() * fonts.size());
-
-            myTeam.setText(currentUser.getTeam().getName());
-            if (currentUser.getTeam().getName().contains("Gabe"))
-                myTeam.setFont(new Font("Comic Sans MS", Font.BOLD, 28));
-            else
-                myTeam.setFont(fonts.get(randomFont));
-            driver1Text.setText(currentUser.getTeam().getDriver1().getName());
-            driver2Text.setText(currentUser.getTeam().getDriver2().getName());
-        }
-
-        ActionListener actionListener = event -> {
-            IUI chooseTeam = new ChooseTeamUI(formulaOne);
-            chooseTeam.updatePane(chooseTeam);
-        };
-        changeTeamButton.addActionListener(actionListener);
+        setListData();
+        setOnClick();
+        setFonts();
     }
 
     public void getData(MenuUI data) {
@@ -132,6 +77,84 @@ public class MenuUI extends AUI {
             FormulaOne.getPage().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             FormulaOne.getPage().pack();
             FormulaOne.getPage().setVisible(true);
+        }
+    }
+
+    private void setListData() {
+        ArrayList<ITeam> teams = formulaOne.getSessionCache().getTeams(); //TODO: maybe change to teamleaderboard class
+        ArrayList<IDriver> drivers = new ArrayList<>();
+        ArrayList<String> values = new ArrayList<>(); //This can also be done with the team class...
+        // Add header
+        values.add("Points | Team | Driver 1 | Driver 2 | Car");
+        for (ITeam team : teams) {
+            values.add(
+                    team.getPoints() + " | " +
+                            team.getName() + " | " +
+                            team.getDriver1().getName() + " | " +
+                            team.getDriver2().getName() + " | " +
+                            team.getCar().getName()
+            );
+            drivers.add(team.getDriver1());
+            drivers.add(team.getDriver2());
+        }
+        teamLeaderboard.setListData(values.toArray());
+
+        ArrayList<String> driverValues = new ArrayList<>();
+        driverValues.add("Points | Driver | Team ID | Experience | Acceleration | Consistency | Cornering");
+        for (IDriver driver : drivers) {
+            driverValues.add(
+                    driver.getPoints() + " | " +
+                            driver.getName() + " | " +
+                            driver.getTeamID() + " | " +
+                            driver.getExperience() + " | " +
+                            driver.getAcceleration() + " | " +
+                            driver.getConsistency() + " | " +
+                            driver.getCorner()
+            );
+        }
+        driverLeaderboard.setListData(driverValues.toArray());
+    }
+
+    private void setOnClick() {
+        ActionListener actionListener = event -> {
+            IUI chooseTeam = new ChooseTeamUI(formulaOne);
+            chooseTeam.updatePane(chooseTeam);
+        };
+        changeTeamButton.addActionListener(actionListener);
+    }
+    private void setFonts() {
+        if (currentUser != null) {
+            ArrayList<Font> fonts = new ArrayList<>();
+            fonts.add(new Font("Comic Sans MS", Font.BOLD, 28));
+            //Add more fonts with same size here
+            fonts.add(new Font("Arial", Font.BOLD, 28));
+            fonts.add(new Font("Times New Roman", Font.BOLD, 28));
+            fonts.add(new Font("Verdana", Font.BOLD, 28));
+            fonts.add(new Font("Tahoma", Font.BOLD, 28));
+            fonts.add(new Font("Calibri", Font.BOLD, 28));
+
+            ArrayList<Font> smallfonts = new ArrayList<>();
+            smallfonts.add(new Font("Comic Sans MS", Font.PLAIN, 12));
+            smallfonts.add(new Font("Arial", Font.PLAIN, 12));
+            smallfonts.add(new Font("Times New Roman", Font.PLAIN, 12));
+            smallfonts.add(new Font("Verdana", Font.PLAIN, 12));
+            smallfonts.add(new Font("Tahoma", Font.PLAIN, 12));
+            smallfonts.add(new Font("Calibri", Font.PLAIN, 12));
+
+            myTeam.setText(currentUser.getTeam().getName());
+            if (currentUser.getTeam().getName().contains("Gabe"))
+                myTeam.setFont(new Font("Comic Sans MS", Font.BOLD, 28));
+            else
+                myTeam.setFont(fonts.get((int) (Math.random() * fonts.size())));
+
+            driver1Text.setText(currentUser.getTeam().getDriver1().getName());
+            driver1Text.setFont(fonts.get((int) (Math.random() * fonts.size())));
+
+            driver2Text.setText(currentUser.getTeam().getDriver2().getName());
+            driver2Text.setFont(fonts.get((int) (Math.random() * fonts.size())));
+
+            driverLeaderboard.setFont(smallfonts.get((int) (Math.random() * smallfonts.size())));
+            teamLeaderboard.setFont(smallfonts.get((int) (Math.random() * smallfonts.size())));
         }
     }
 
