30f8046ed380ba8dfefe3bfd60ca43c6c5f7755d
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-14 09:36:54 +0100

    small naming changes

--- src/main/race/impl/RaceAlgorithmStraightImpl.java
+++ src/main/race/impl/RaceAlgorithmStraightImpl.java
@@ -11,19 +11,19 @@ public class RaceAlgorithmStraightImpl implements IRaceAlgorithm {
 	double currentAcceleration; // current acceleration of the car measured in m/s^2
 	double distance = 0; // distance traveled by the car measured in m
 	double time = 0; //current time in this circuit measured in s
-	double dtime = 0.1; //time step measured in s
+	double dTime = 0.1; //time step measured in s
 
 	public RaceAlgorithmStraightImpl() { }
 
 	private double getTime(ICar car, CircuitComponentStraightImpl circuit) {
-		double maxdistance = circuit.getLength(); //get length of circuit
+		double maxDistance = circuit.getLength(); //get length of circuit
 		double time = 0;
 
-		while (distance <= maxdistance) {
+		while (distance <= maxDistance) {
 			currentAcceleration = getAcceleration(car, currentSpeed); //get acceleration from the car
 			currentSpeed = getSpeed(currentSpeed, currentAcceleration); //	get speed from the car
-			distance = distance + currentSpeed * dtime; //calculate distance traveled
-			time = time + dtime; //calculate time
+			distance = distance + currentSpeed * dTime; //calculate distance traveled
+			time = time + dTime; //calculate time
 		}
 		return time;
 	}
@@ -32,13 +32,12 @@ public class RaceAlgorithmStraightImpl implements IRaceAlgorithm {
 		int power = car.getHorsePower(); // 1000 hp
 		double mass = car.getWeight(); // 1000 kg
 		double drag = car.getAerodynamics(); // 10% drag
-		double rollresistance = car.getTraction(); // 10% roll resistance
-		double acceleration = (power - drag * currentSpeed * currentSpeed - rollresistance * currentSpeed) / mass; // F = ma -> a = F/m -> a = (P - Dv^2 - Rv) / m
-		return acceleration;
+		double rollResistance = car.getTraction(); // 10% roll resistance
+		return (power - drag * currentSpeed * currentSpeed - rollResistance * currentSpeed) / mass; // F = ma -> a = F/m -> a = (P - Dv^2 - Rv) / m
 	}
 
 	private double getSpeed(double currentSpeed, double currentAcceleration) {
-		currentSpeed = currentSpeed + currentAcceleration * dtime;
+		currentSpeed = currentSpeed + currentAcceleration * dTime;
 		return currentSpeed;
 	}
 }
