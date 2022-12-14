a1a0f30a336fd04142db0fd50aa69c88e4a14967
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-14 09:44:14 +0100

    created class for test circuit

---
+++ src/test/TestRaceCircuitFunction.java
@@ -0,0 +1,51 @@
+package test;
+
+import main.data.ICar;
+import main.data.IDriver;
+import main.data.ITeam;
+import main.data.impl.CarImpl;
+import main.data.impl.DriverImpl;
+import main.data.impl.TeamImpl;
+import main.race.ICircuit;
+import main.race.IRace;
+import main.race.impl.CircuitImpl;
+import main.race.impl.RaceImpl;
+
+import java.util.ArrayList;
+
+public class TestRaceCircuitFunction {
+
+    public static void main(String[] args) {
+        TestRaceCircuitFunction testRaceCircuitFunction = new TestRaceCircuitFunction();
+        testRaceCircuitFunction.run();
+    }
+
+    public void run() {
+        ArrayList<ITeam> teams = getSomeDefaultTeamData();
+
+        ICircuit circuit1 = getCircuit();
+
+        IRace race1 = new RaceImpl(2022, circuit1, teams);
+
+        race1.nextAction();
+        race1.nextAction();
+    }
+
+    public ArrayList<ITeam> getSomeDefaultTeamData() {
+        ArrayList<ITeam> teams = new ArrayList<>();
+
+        // create 10 teams in total
+        for (int i = 1; i <= 10; i++) {
+            ICar car = new CarImpl(i, "car" + i, 1, 1, 1, 1);
+            IDriver driver1 = new DriverImpl(i * 2 - 1, "driver" + (i * 2 - 1), i, 1, 1, 1, 1);
+            IDriver driver2 = new DriverImpl(i * 2, "driver" + (i * 2), i, 1, 1, 1, 1);
+            teams.add(new TeamImpl(i, "team" + i, car, driver1, driver2));
+        }
+
+        return teams;
+    }
+
+    public ICircuit getCircuit() {
+        return new CircuitImpl(2022, "Circuit1", "Country1", 58, null);
+    }
+}
