5194b6f3fda5a9038c0916facb327e96c17a0264
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-15 11:24:04 +0100

    spacing

--- src/main/database/mysql/IMySQL.java
+++ src/main/database/mysql/IMySQL.java
@@ -6,12 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface IMySQL {
+
     boolean openConnection(String url, String name, String password) throws SQLException, ClassNotFoundException;
+
     boolean openConnection(String IP, String Schema, String name, String password) throws SQLException, ClassNotFoundException;
+
     boolean closeConnection();
+
     ResultSet executeQuery(String query);
+
     boolean executeChange(PreparedStatement query);
 
     Connection getConnection();
-
 }
