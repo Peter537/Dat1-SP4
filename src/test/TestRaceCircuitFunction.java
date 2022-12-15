a184a63df45a4df8d50f77fc30bfd53e492a2526
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-12-15 09:50:43 +0100

    update test to get more precise numbers

--- src/test/TestRaceCircuitFunction.java
+++ src/test/TestRaceCircuitFunction.java
@@ -43,8 +43,8 @@ public class TestRaceCircuitFunction {
         // create 10 teams in total
         for (int i = 1; i <= 10; i++) {
             ICar car = new CarImpl(i, "car" + i, r.nextInt(50) + 1000, r.nextInt(50) + 1000, r.nextDouble(0.5) + 0.5, r.nextDouble(0.5) + 0.5);
-            IDriver driver1 = new DriverImpl(i * 2 - 1, "driver" + (i * 2 - 1), 1, 1, 1, 1);
-            IDriver driver2 = new DriverImpl(i * 2, "driver" + (i * 2), 1, 1, 1, 1);
+            IDriver driver1 = new DriverImpl(i * 2 - 1, "driver" + (i * 2 - 1), r.nextInt(20) + 60, r.nextInt(20) + 60, r.nextInt(20) + 60, r.nextInt(20) + 60);
+            IDriver driver2 = new DriverImpl(i * 2, "driver" + (i * 2), r.nextInt(20) + 60, r.nextInt(20) + 60, r.nextInt(20) + 60, r.nextInt(20) + 60);
             ITeam team = new TeamImpl(i, "team" + i, car, driver1, driver2);
             driver1.setTeam(team);
             driver2.setTeam(team);
